c19f7a26e24a80aa6472b2caecf0ff00b68a2cf4
Lee Pham <leepham@utexas.edu>
2022-02-16 11:28:15 -0600

    replace most files with other repo

---
+++ gcal/gcal.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This is where we retrieve events from the Google Calendar. Before doing so, make sure you have both the
+credentials.json and token.pickle in the same folder as this file. If not, run quickstart.py first.
+"""
+
+from __future__ import print_function
+
+import datetime as dt
+import logging
+import os.path
+import pathlib
+import pickle
+
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+
+class GcalHelper:
+
+    def __init__(self):
+        self.logger = logging.getLogger('maginkcal')
+        # Initialise the Google Calendar using the provided credentials and token
+        SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+        self.currPath = str(pathlib.Path(__file__).parent.absolute())
+
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists(self.currPath + '/token.pickle'):
+            with open(self.currPath + '/token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.currPath + '/credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(self.currPath + '/token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        self.service = build('calendar', 'v3', credentials=creds, cache_discovery=False)
+
+    def list_calendars(self):
+        # helps to retrieve ID for calendars within the account
+        # calendar IDs added to config.json will then be queried for retrieval of events
+        self.logger.info('Getting list of calendars')
+        calendars_result = self.service.calendarList().list().execute()
+        calendars = calendars_result.get('items', [])
+        if not calendars:
+            self.logger.info('No calendars found.')
+        for calendar in calendars:
+            summary = calendar['summary']
+            cal_id = calendar['id']
+            self.logger.info("%s\t%s" % (summary, cal_id))
+
+    def to_datetime(self, isoDatetime, localTZ):
+        # replace Z with +00:00 is a workaround until datetime library decides what to do with the Z notation
+        toDatetime = dt.datetime.fromisoformat(isoDatetime.replace('Z', '+00:00'))
+        return toDatetime.astimezone(localTZ)
+
+    def is_recent_updated(self, updatedTime, thresholdHours):
+        # consider events updated within the past X hours as recently updated
+        utcnow = dt.datetime.now(dt.timezone.utc)
+        diff = (utcnow - updatedTime).total_seconds() / 3600  # get difference in hours
+        return diff < thresholdHours
+
+    def adjust_end_time(self, endTime, localTZ):
+        # check if end time is at 00:00 of next day, if so set to max time for day before
+        if endTime.hour == 0 and endTime.minute == 0 and endTime.second == 0:
+            newEndtime = localTZ.localize(
+                dt.datetime.combine(endTime.date() - dt.timedelta(days=1), dt.datetime.max.time()))
+            return newEndtime
+        else:
+            return endTime
+
+    def is_multiday(self, start, end):
+        # check if event stretches across multiple days
+        return start.date() != end.date()
+
+    def retrieve_events(self, calendars, startDatetime, endDatetime, localTZ, thresholdHours):
+        # Call the Google Calendar API and return a list of events that fall within the specified dates
+        eventList = []
+
+        minTimeStr = startDatetime.isoformat()
+        maxTimeStr = endDatetime.isoformat()
+        if False:
+            return eventList
+
+        self.logger.info('Retrieving events between ' + minTimeStr + ' and ' + maxTimeStr + '...')
+        events_result = []
+        for cal in calendars:
+            events_result.append(
+                self.service.events().list(calendarId=cal, timeMin=minTimeStr,
+                                           timeMax=maxTimeStr, singleEvents=True,
+                                           orderBy='startTime').execute()
+            )
+
+        events = []
+        for eve in events_result:
+            events += eve.get('items', [])
+            # events = events_result.get('items', [])
+
+        if not events:
+            self.logger.info('No upcoming events found.')
+        for event in events:
+            # extracting and converting events data into a new list
+            newEvent = {}
+            newEvent['summary'] = event['summary']
+
+            if event['start'].get('dateTime') is None:
+                newEvent['allday'] = True
+                newEvent['startDatetime'] = self.to_datetime(event['start'].get('date'), localTZ)
+            else:
+                newEvent['allday'] = False
+                newEvent['startDatetime'] = self.to_datetime(event['start'].get('dateTime'), localTZ)
+
+            if event['end'].get('dateTime') is None:
+                newEvent['endDatetime'] = self.adjust_end_time(self.to_datetime(event['end'].get('date'), localTZ),
+                                                               localTZ)
+            else:
+                newEvent['endDatetime'] = self.adjust_end_time(self.to_datetime(event['end'].get('dateTime'), localTZ),
+                                                               localTZ)
+
+            newEvent['updatedDatetime'] = self.to_datetime(event['updated'], localTZ)
+            newEvent['isUpdated'] = self.is_recent_updated(newEvent['updatedDatetime'], thresholdHours)
+            newEvent['isMultiday'] = self.is_multiday(newEvent['startDatetime'], newEvent['endDatetime'])
+            eventList.append(newEvent)
+
+        # We need to sort eventList because the event will be sorted in "calendar order" instead of hours order
+        # TODO: improve because of double cycle for now is not much cost
+        eventList = sorted(eventList, key=lambda k: k['startDatetime'])
+        return eventList
---
+++ render/render.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This script essentially generates a HTML file of the calendar I wish to display. It then fires up a headless Chrome
+instance, sized to the resolution of the eInk display and takes a screenshot. This screenshot will then be processed
+to extract the grayscale and red portions, which are then sent to the eInk display for updating.
+
+This might sound like a convoluted way to generate the calendar, but I'm doing so mainly because (i) it's easier to
+format the calendar exactly the way I want it using HTML/CSS, and (ii) I can better delink the generation of the
+calendar and refreshing of the eInk display. In the future, I might choose to generate the calendar on a separate
+RPi device, while using a ESP32 or PiZero purely to just retrieve the image from a file host and update the screen.
+"""
+
+import logging
+import pathlib
+from datetime import timedelta
+from time import sleep
+
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class RenderHelper:
+
+    def __init__(self, width, height, angle):
+        self.logger = logging.getLogger('maginkcal')
+        self.currPath = str(pathlib.Path(__file__).parent.absolute())
+        self.htmlFile = 'file://' + self.currPath + '/calendar.html'
+        self.imageWidth = width
+        self.imageHeight = height
+        self.rotateAngle = angle
+
+    def set_viewport_size(self, driver):
+
+        # Extract the current window size from the driver
+        current_window_size = driver.get_window_size()
+
+        # Extract the client window size from the html tag
+        html = driver.find_element_by_tag_name('html')
+        inner_width = int(html.get_attribute("clientWidth"))
+        inner_height = int(html.get_attribute("clientHeight"))
+
+        # "Internal width you want to set+Set "outer frame width" to window size
+        target_width = self.imageWidth + (current_window_size["width"] - inner_width)
+        target_height = self.imageHeight + (current_window_size["height"] - inner_height)
+
+        driver.set_window_rect(
+            width=target_width,
+            height=target_height)
+
+    def get_screenshot(self):
+        opts = Options()
+        opts.add_argument("--hide-scrollbars");
+        opts.add_argument('--force-device-scale-factor=1')
+        driver = webdriver.Chrome(options=opts)
+        self.set_viewport_size(driver)
+        driver.get(self.htmlFile)
+        sleep(1)
+        driver.get_screenshot_as_file(self.currPath + '/calendar.png')
+        driver.quit()
+
+        self.logger.info('Screenshot captured and saved to file.')
+
+        redimg = Image.open(self.currPath + '/calendar.png')  # get image)
+        rpixels = redimg.load()  # create the pixel map
+        blackimg = Image.open(self.currPath + '/calendar.png')  # get image)
+        bpixels = blackimg.load()  # create the pixel map
+
+        for i in range(redimg.size[0]):  # loop through every pixel in the image
+            for j in range(redimg.size[1]):  # since both bitmaps are identical, cycle only once and not both bitmaps
+                if rpixels[i, j][0] <= rpixels[i, j][1] and rpixels[i, j][0] <= rpixels[i, j][2]:  # if is not red
+                    rpixels[i, j] = (255, 255, 255)  # change it to white in the red image bitmap
+
+                elif bpixels[i, j][0] > bpixels[i, j][1] and bpixels[i, j][0] > bpixels[i, j][2]:  # if is red
+                    bpixels[i, j] = (255, 255, 255)  # change to white in the black image bitmap
+
+        redimg = redimg.rotate(self.rotateAngle, expand=True)
+        blackimg = blackimg.rotate(self.rotateAngle, expand=True)
+
+        self.logger.info('Image colours processed. Extracted grayscale and red images.')
+        return blackimg, redimg
+
+    def get_day_in_cal(self, startDate, eventDate):
+        delta = eventDate - startDate
+        return delta.days
+
+    def get_short_time(self, datetimeObj, is24hour=False):
+        datetime_str = ''
+        if is24hour:
+            datetime_str = '{}:{:02d}'.format(datetimeObj.hour, datetimeObj.minute)
+        else:
+            if datetimeObj.minute > 0:
+                datetime_str = '.{:02d}'.format(datetimeObj.minute)
+
+            if datetimeObj.hour == 0:
+                datetime_str = '12{}am'.format(datetime_str)
+            elif datetimeObj.hour == 12:
+                datetime_str = '12{}pm'.format(datetime_str)
+            elif datetimeObj.hour > 12:
+                datetime_str = '{}{}pm'.format(str(datetimeObj.hour % 12), datetime_str)
+            else:
+                datetime_str = '{}{}am'.format(str(datetimeObj.hour), datetime_str)
+        return datetime_str
+
+    def process_inputs(self, calDict):
+        # calDict = {'events': eventList, 'calStartDate': calStartDate, 'today': currDate, 'lastRefresh': currDatetime, 'batteryLevel': batteryLevel}
+        # first setup list to represent the 5 weeks in our calendar
+        calList = []
+        for i in range(35):
+            calList.append([])
+
+        # retrieve calendar configuration
+        maxEventsPerDay = calDict['maxEventsPerDay']
+        batteryDisplayMode = calDict['batteryDisplayMode']
+        dayOfWeekText = calDict['dayOfWeekText']
+        weekStartDay = calDict['weekStartDay']
+        is24hour = calDict['is24hour']
+
+        # for each item in the eventList, add them to the relevant day in our calendar list
+        for event in calDict['events']:
+            idx = self.get_day_in_cal(calDict['calStartDate'], event['startDatetime'].date())
+            if idx >= 0:
+                calList[idx].append(event)
+            if event['isMultiday']:
+                idx = self.get_day_in_cal(calDict['calStartDate'], event['endDatetime'].date())
+                if idx < len(calList):
+                    calList[idx].append(event)
+
+        # Read html template
+        with open(self.currPath + '/calendar_template.html', 'r') as file:
+            calendar_template = file.read()
+
+        # Insert month header
+        month_name = str(calDict['today'].month)
+
+        # Insert battery icon
+        # batteryDisplayMode - 0: do not show / 1: always show / 2: show when battery is low
+        # battLevel = calDict['batssssteryLevel']
+
+        # Populate the day of week row
+        cal_days_of_week = ''
+        for i in range(0, 7):
+            cal_days_of_week += '<li class="font-weight-bold text-uppercase">' + dayOfWeekText[
+                (i + weekStartDay) % 7] + "</li>\n"
+
+        # Populate the date and events
+        cal_events_text = ''
+        for i in range(len(calList)):
+            currDate = calDict['calStartDate'] + timedelta(days=i)
+            dayOfMonth = currDate.day
+            if currDate == calDict['today']:
+                cal_events_text += '<li><div class="datecircle">' + str(dayOfMonth) + '</div>\n'
+            elif currDate.month != calDict['today'].month:
+                cal_events_text += '<li><div class="date text-muted">' + str(dayOfMonth) + '</div>\n'
+            else:
+                cal_events_text += '<li><div class="date">' + str(dayOfMonth) + '</div>\n'
+
+            for j in range(min(len(calList[i]), maxEventsPerDay)):
+                event = calList[i][j]
+                cal_events_text += '<div class="event'
+                if event['isUpdated']:
+                    cal_events_text += ' text-danger'
+                elif currDate.month != calDict['today'].month:
+                    cal_events_text += ' text-muted'
+                if event['isMultiday']:
+                    if event['startDatetime'].date() == currDate:
+                        cal_events_text += '">►' + event['summary']
+                    else:
+                        # calHtmlList.append(' text-multiday">')
+                        cal_events_text += '">◄' + event['summary']
+                elif event['allday']:
+                    cal_events_text += '">' + event['summary']
+                else:
+                    cal_events_text += '">' + self.get_short_time(event['startDatetime'], is24hour) + ' ' + event[
+                        'summary']
+                cal_events_text += '</div>\n'
+            if len(calList[i]) > maxEventsPerDay:
+                cal_events_text += '<div class="event text-muted">' + str(len(calList[i]) - maxEventsPerDay) + ' more'
+
+            cal_events_text += '</li>\n'
+
+        # Append the bottom and write the file
+        htmlFile = open(self.currPath + '/calendar.html', "w")
+        htmlFile.write(calendar_template.format(month=month_name, dayOfWeek=cal_days_of_week,
+                                                events=cal_events_text))
+        htmlFile.close()
+
+        calBlackImage, calRedImage = self.get_screenshot()
+
+        return calBlackImage, calRedImage
