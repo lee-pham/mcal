8cf589f6771b8f569a3ba2ecbec9806b04e28fef
Lee Pham <lee.pham@libertymutual.com>
2022-02-23 17:07:21 -0500

    adjust timeline aesthetic

--- timeline/timeline.py
+++ timeline/timeline.py
@@ -6,7 +6,7 @@ event_list = [
     {"summary": "Dentist", "start": "08:00", "end": "09:00"},
     {"summary": "Work", "start": "08:30", "end": "09:00"},
     {"summary": "Lunch", "start": "12:00", "end": "13:00"},
-    {"summary": "coffee", "start": "12:15", "end": "12:30"},
+    {"summary": "Coffee", "start": "12:15", "end": "12:30"},
     {"summary": "Pottery", "start": "18:00", "end": "21:00"},
     {"summary": "Lee", "start": "22:00", "end": "23:59"},
 ]
@@ -28,20 +28,20 @@ def is_complete(event: dict) -> dict:
         return {
             "color": "#6c757d",
             "markerfacecolor": GRAY,
-            "xy": .005
+            "xy": .01
         }
     else:
         # incomplete
         return {
             "color": "k",
             "markerfacecolor": "w",
-            "xy": -.0075
+            "xy": -.01
         }
 
 
 # Create figure and plot a stem plot with the date
-fig, ax = plt.subplots(figsize=(8.8, 4), constrained_layout=True)
-
+# fig, ax = plt.subplots(figsize=(8.8, 4), constrained_layout=True)
+fig, ax = plt.subplots(figsize=(8.8, 4), constrained_layout=False)
 current_time = datetime.now().strftime("%H:%M")
 
 # Black line for the entire timeline
@@ -55,17 +55,23 @@ ax.plot(military_to_minutes(current_time), 0, "|", color="r")
 texts = []
 for event in event_list:
     summary_with_time = f'{event["start"]} {event["summary"]}'
-    ax.plot(military_to_minutes(event["start"]), 0, "o",
-            color=is_complete(event)["color"], markerfacecolor=is_complete(event)["markerfacecolor"])
-    texts.append(ax.text(s=summary_with_time, x=military_to_minutes(event["start"]), y=is_complete(event)["xy"],
-                color=is_complete(event)["color"]))
+
+    ax.plot(
+        military_to_minutes(event["start"]), 0,
+        "o", color=is_complete(event)["color"], markerfacecolor=is_complete(event)["markerfacecolor"])
+
+    texts.append(
+        ax.text(s=summary_with_time,
+                x=military_to_minutes(event["start"]), y=is_complete(event)["xy"],
+                color=is_complete(event)["color"])
+    )
 
 # remove x, y axis and spines
 ax.xaxis.set_visible(False)
 ax.yaxis.set_visible(False)
 ax.spines[["left", "top", "right", "bottom"]].set_visible(False)
-
 ax.margins(y=0.1)
-adjust_text(texts)
 
+adjust_text(texts, only_move={'points': 'y', 'text': 'y', 'objects': 'y'}, ha='left', va='bottom')
+# adjust_text(texts)
 plt.show()
