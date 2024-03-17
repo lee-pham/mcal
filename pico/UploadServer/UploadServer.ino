e656174cd85c8157e300745539c14130fce2a95b
Lee Pham <leepham@utexas.edu>
2024-03-17 02:25:33 -0500

    send 92kb to pico server

--- pico/UploadServer/UploadServer.ino
+++ pico/UploadServer/UploadServer.ino
@@ -4,31 +4,53 @@
 #include <LEAmDNS.h>
 
 #ifndef STASSID
-#define STASSID "ssid"
-#define STAPSK "passw"
+#define STASSID "name"
+#define STAPSK "pass"
 #endif
 
-const char* ssid = STASSID;
-const char* password = STAPSK;
+
+const char *ssid = STASSID;
+const char *password = STAPSK;
 
 WebServer server(80);
 
 const int led = LED_BUILTIN;
-
+#include <EPD_Driver.h>
 
 void handleUpload() {
-  digitalWrite(led, 0);
-  Serial.println(server.upload().filename);
-  Serial.println(server.upload().totalSize);
-  // Serial.write(server.upload().buf, server.upload().totalSize);
-  server.send(200, "text/plain", "thanks for the upload!\r\n");
-  digitalWrite(led, 1);
-}
+  Serial.println("Header values");
+  Serial.println("User-Agent: " + server.header("User-Agent"));
+  Serial.println("Content-Type: " + server.header("Content-Type"));
+  String contentLength = server.header("Content-Length");
+  Serial.println("Content-Length: " + contentLength);
+  Serial.println();
 
+  int numArgs = server.args();
+  Serial.println("Num of args: " + String(numArgs));
+  for (int i = 0; i <= numArgs; i++) {
+    Serial.println(server.argName(i));    
+  }
+
+  String body = server.arg("plain");
+  int frame[46080];
+  for (int i = 0; i <= contentLength.toInt(); i += 2) {
+    int pixels = strtol(body.substring(i, i + 2).c_str(), NULL, 16);
+    frame[i/2] = pixels;
+  }
+
+  server.send(200, "text/plain", "200 OK");
+  Serial.println();
+
+  // EPD_Driver epdtest(eScreen_EPD_B98, boardRaspberryPiPico_RP2040_EXT3);
+  // epdtest.globalUpdate(data, data, data, data);
+  // Turn off CoG`
+  // epdtest.COG_powerOff();
+}
 void setup(void) {
+  Serial.begin(115200);
+  Serial.println();
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
-  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
@@ -41,6 +63,7 @@ void setup(void) {
     delay(250);
     Serial.print(".");
   }
+
   digitalWrite(led, 1);
   Serial.println("");
   Serial.print("Connected to ");
@@ -53,9 +76,9 @@ void setup(void) {
   }
 
   server.on("/", handleUpload);
-  
-  // server.onFileUpload(handleUpload);
+  server.onFileUpload(handleUpload);
 
+  server.collectHeaders("User-Agent", "Content-Type", "Content-Length");
   server.begin();
   Serial.println("HTTP server started");
 }
--- utils/upload_to_server.py
+++ utils/upload_to_server.py
@@ -1,14 +1,30 @@
 import requests
-from PIL import Image
+import sys
+import zlib
 from epd_image_processor import convert_image_to_list_of_bytes
+from PIL import Image
+
+url = "http://picow.local/"
 
 
-files = {
-    "file": convert_image_to_list_of_bytes(Image.open("../render/calendar.png"), (768, 960), 2)[0]
+payload = convert_image_to_list_of_bytes(
+    Image.open("test_image.png"),
+    (768, 960),
+    2
+)[0].hex()
+
+
+
+headers = {
+    "User-Agent": "framework/upload_to_server.py",
+    "Content-Type": "application/octet-stream",
+    # Content-Length is calculated and included by the requests library
 }
 
-r = requests.post(
-    "http://picow.local",
-    files=files
-    )
-print(r.text)
+response = requests.request(
+    "PUT",
+    url,
+    data=payload,
+    headers=headers
+)
+print(response.text)
