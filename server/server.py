6208b82dff5d06fdced2856d01187c23415f033f
Lee Pham <leepham@utexas.edu>
2025-03-12 02:11:13 -0500

    test server update

--- server/server.py
+++ server/server.py
@@ -2,7 +2,15 @@ from flask import Flask, Response
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/data")
 def index():
-    text = (("a" * 46079) + "e") * 1
-    return Response(text, mimetype="application/octet-stream")
+    text = (("a" * 46080*2) + "e") * 1
+    # text = "HELO worldddd".encode()
+    # response = Response(text, mimetype="application/octet-stream")
+    binary_data = bytes([0x01, 0x02, 0x03, 0x04, 0x00, 0x05, 0x06, 0x07, 0x08])
+    # Send the raw binary data directly in the response
+    return Response(text, content_type='application/octet-stream')
+
+app.run(host="0.0.0.0",
+        port=5000,
+        debug=True)
